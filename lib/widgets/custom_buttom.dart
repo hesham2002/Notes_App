73961d7a06fe6db30fe7648cd1ba8e3cc5973658
hesham2002 <heshamgharib2002@gmail.com>
2023-09-12 19:05:35 +0300

    create Custom_Bottom

---
+++ lib/widgets/custom_buttom.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomButtom extends StatelessWidget {
+  const CustomButtom({super.key, required this.buttomName});
+ final String buttomName ;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.cyan,
+        borderRadius: BorderRadius.circular(10),
+      ),
+
+      height: 50,
+      width: double.infinity,
+      child: Center(
+          child: Text(
+            buttomName,
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          )),
+    );
+  }
+}
